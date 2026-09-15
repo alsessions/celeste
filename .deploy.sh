2bdfc0c9f852d850b49de33872c02d761507f04d
alsessions <fultonchain@gmail.com>
2026-09-15 19:35:51 -0400

    Deployment script.

--- .deploy.sh
+++ .deploy.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+git pull --ff-only origin main
+
 composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
 php craft up --interactive=0
 php craft migrate/all --interactive=0
